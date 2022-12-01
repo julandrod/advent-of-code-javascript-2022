5d03d75241e2eb652cd5a2d45773dce0e5a48bd0
julandrod <julian.andres.rodriguez@gmail.com>
2022-12-01 11:50:40 -0500

    day 01

--- src/day01/part1.js
+++ src/day01/part1.js
@@ -1,5 +1,11 @@
 // Advent of Code - Day 1 - Part One
 
 export function part1(input) {
-  return 0;
+  const inputText = input.split(/\n\n/);
+
+  const elfCalories = inputText.map((input) => {
+    return input.split(/\n/).reduce((acc, cur) => acc + parseInt(cur, 10), 0);
+  });
+
+  return Math.max(...elfCalories);
 }
--- src/day01/part2.js
+++ src/day01/part2.js
@@ -1,5 +1,16 @@
 // Advent of Code - Day 1 - Part Two
 
 export function part2(input) {
-  return 0;
+  const inputText = input.split(/\n\n/);
+
+  const elfCalories = inputText.map((input) => {
+    return input.split(/\n/).reduce((acc, cur) => acc + parseInt(cur, 10), 0);
+  });
+
+  const caloriesSort = elfCalories
+    .sort((a, b) => b - a)
+    .splice(0, 3)
+    .reduce((acc, cur) => cur + acc, 0);
+
+  return caloriesSort;
 }
